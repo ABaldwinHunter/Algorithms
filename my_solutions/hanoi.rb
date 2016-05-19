8a1e67a36d90b52d601d807f60aa92f662bbc407
ABaldwinHunter <abaldwinhunter@codeclimate.com>
2016-05-18 20:25:19 -0400

    Hanoi WIP

---
+++ my_solutions/hanoi.rb
@@ -0,0 +1,24 @@
+# rods = []
+# ring = integers 1 -> n
+
+def hanoi(n, from, to, interim)
+  if n == 1
+  if a.length
+
+  display(rods)
+end
+
+
+def display(rods)
+  spaces = " "*10
+  puts "A" + spaces + "B" + spaces + "C" + spaces
+  rods.each do |r|
+    print r.to_s + " "*5
+  end
+  puts
+end
+
+hanoi(n, [], [], [])
+
+
+# test cases
